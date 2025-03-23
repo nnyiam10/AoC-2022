38b675a81ef736ed4e18ec3f3c33014c1c8ace36
Nten Nyiam <nten.nyiam@gmail.com>
2025-03-23 12:04:39 -0400

    Update 2022 files

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,25 +1,30 @@
-filename = 'input.txt'
-puzzle_input = [r.strip() for r in open(filename).readlines()]
+from heapq import nlargest
 
 def top_three(input):
-    a,b,c = 0,0,0
+    calorie_totals = []
     curr_cals = 0
 
-    for entry in input:
-        if entry == '':
-            if curr_cals > a:
-                a,b,c = curr_cals,a,b
-            elif curr_cals > b:
-                b,c = curr_cals,b
-            elif curr_cals > c:
-                c = curr_cals
+    for line in input:
+        if line == "":
+            calorie_totals.append(curr_cals)
             curr_cals = 0
         else:
-            curr_cals += int(entry)
-    return a,b,c
+            curr_cals += int(line)
 
-#Part 1
-print(max(top_three(puzzle_input)))
+    if curr_cals > 0:
+        calorie_totals.append(curr_cals)
+    return nlargest(3, calorie_totals)
 
-# Part 2
-print(sum(top_three(puzzle_input)))
+def main():
+    filename = "2022/input.txt"
+    with open(filename, "r") as f:
+        puzzle_input = [line.strip() for line in f]
+
+    #Part 1
+    print(max(top_three(puzzle_input)))
+
+    # Part 2
+    print(sum(top_three(puzzle_input)))
+
+if __name__ == "__main__":
+    main()
