354e87cd49326546fa62deb0534132f5ae5af8a7
Nten Nyiam <nten.nyiam@gmail.com>
2025-04-08 03:20:35 -0400

    updated day2

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,27 +1,35 @@
-filename = 'input.txt'
+filename = "2022/input.txt"
 puzzle_input = [r.strip() for r in open(filename).readlines()]
-shape_score = {'X': 1, 'Y': 2, 'Z': 3}
+
 
 def part1(puzzle_input):
-    opponent_score = {'A': {'X': 3, 'Y': 6, 'Z': 0}, 'B': {'X': 0, 'Y': 3, 'Z': 6}, 'C': {'X': 6, 'Y': 0, 'Z': 3}}
-    total_score = 0
-    for input in puzzle_input:
-        opponent, you = input.split()
-        total_score += shape_score[you] + opponent_score[opponent][you]
-    return total_score
+    shape_score = {'X': 1, 'Y': 2, 'Z': 3}
+    opponent_score = {
+        'A': {'X': 3, 'Y': 6, 'Z': 0}, 
+        'B': {'X': 0, 'Y': 3, 'Z': 6}, 
+        'C': {'X': 6, 'Y': 0, 'Z': 3}
+    }
+    return sum(shape_score[you] + opponent_score[opponent][you] for opponent, you in map(str.split, puzzle_input))
 
 def part2(puzzle_input):
-    total_score = 0
-    opposite_shape_score = {'A': {'X': 3 , 'Y': 1 , 'Z': 2 }, 'B': {'X': 1 , 'Y': 2 , 'Z': 3 }, 'C': {'X': 2, 'Y': 3, 'Z': 1}}
+    opposite_shape_score = {
+        'A': {'X': 3, 'Y': 1, 'Z': 2},
+        'B': {'X': 1, 'Y': 2, 'Z': 3},
+        'C': {'X': 2, 'Y': 3, 'Z': 1}
+    }
     your_score = {'X': 0, 'Y': 3, 'Z': 6}
-    for input in puzzle_input:
-        opponent, you = input.split()
-        total_score += opposite_shape_score[opponent][you] + your_score[you]
-    return total_score
+    return sum(opposite_shape_score[opponent][you] + your_score[you] for opponent, you in map(str.split, puzzle_input))
 
-#Part 1
-print(part1(puzzle_input))
+def main():
+    filename = "2022/input.txt"
+    with open(filename) as f:
+        puzzle_input = [line.strip() for line in f]
 
-#Part 2
-print(part2(puzzle_input))
-        
+    #Part 1
+    print(part1(puzzle_input))
+
+    # Part 2
+    print(part2(puzzle_input))
+
+if __name__ == "__main__":
+    main()
