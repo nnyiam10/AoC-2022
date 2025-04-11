10b8c12d2fc1ed3f57aba1366894381eb1d76163
Nten Nyiam <nten.nyiam@gmail.com>
2025-04-11 06:53:21 -0400

    day 6 updated

---
+++ 2022/day06.py
@@ -0,0 +1,29 @@
+
+
+def parts(puzzle_input, part=1):
+    seen = set()
+    l = 0
+    offset = 4 if part == 1 else 14
+    for r in range(len(puzzle_input)):
+        if r - l == offset:
+            return r
+        
+        char = puzzle_input[r]
+        if char in seen:
+            while l < r and puzzle_input[l] != char:
+                seen.remove(puzzle_input[l])
+                l += 1
+            l += 1
+        else:
+            seen.add(puzzle_input[r])
+
+def main():
+    filename = "2022/input.txt"
+    with open(filename) as f:
+        puzzle_input = f.read().strip()
+
+    print(parts(puzzle_input, 1))
+    print(parts(puzzle_input, 2))
+
+if __name__ == "__main__":
+    main()
